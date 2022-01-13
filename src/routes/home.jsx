23ef71daaa74689ee604bb72dc70d99b4985b23d
BBrandude <BrandudeClutch1@gmail.com>
2022-01-13 11:00:21 -0500

    neatness

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center h-40 bg-black text-white">
-          <a className="flex items-stretch" href="./">
+        <a className="static left-3 flex items-stretch w-1/12" href="./">
           <img src={insomnia} alt="logo" class="resize h-28 w-28 " />
-          </a>
-          <div className="font-mono text-2xl font-medium"><h1 classname="font-mono"> Insomnia Labs</h1></div>
+        </a>
+        <div className="font-mono text-2xl font-medium"><h1 classname="font-mono"> Insomnia Labs</h1></div>
         <div className="md:flex items-center space-x-8 hidden m-auto w-2/6">
           <SectionButton className="text-white" directName="Home" />
           <SectionButton className="text-white" directName="Products" />
@@ -47,22 +47,22 @@ targets.forEach(function (target) {
 
 
 
-  /*
+/*
 
 export default function Home() {
-  return (
-    <main>
-      <div className="flex h-40 bg-black">
-          <img src={insomnia} alt="logo" class="object-scale-down h-20 w-20" />
-        <div className="md:flex items-center space-x-8 hidden m-auto">
-          <SectionButton className="text-white" directName="Home" />
-          <SectionButton className="text-white" directName="Products" />
-          <SectionButton className="text-white" directName="Socials" />
-        </div>
+return (
+  <main>
+    <div className="flex h-40 bg-black">
+        <img src={insomnia} alt="logo" class="object-scale-down h-20 w-20" />
+      <div className="md:flex items-center space-x-8 hidden m-auto">
+        <SectionButton className="text-white" directName="Home" />
+        <SectionButton className="text-white" directName="Products" />
+        <SectionButton className="text-white" directName="Socials" />
       </div>
-    </main>
+    </div>
+  </main>
 
 
-  )
+)
 }
 */
