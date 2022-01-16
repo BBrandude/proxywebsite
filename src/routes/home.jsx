e0f310fc4bf04cb2670b6042ef7b2eb89760da38
BBrandude <brandudeclutch1@gmail.com>
2022-01-16 01:55:30 -0500

    centered div

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -5,25 +5,27 @@ import React from "react"
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex items-center h-1/4 bg-black text-white">
-        <a className="static left-3 flex items-stretch w-1/12" href="./">
-          <img src={insomnia} alt="logo" class="resize h-28 w-28 " />
-        </a>
-        <div className="font-mono text-2xl font-medium"><h1 classname="font-mono"> Insomnia Labs</h1></div>
-        <div className="md:flex items-center space-x-8 hidden m-auto w-2/6">
-          <SectionButton className="text-white" directName="Home" />
-          <SectionButton className="text-white" directName="Products" />
-          <SectionButton className="text-white" directName="Socials" />
+    <div>
+      <div className="h-screen">
+        <div className="flex items-center bg-black text-white h-1/6">
+          <a className="static left-3 flex items-stretch w-56" href="./">
+            <img src={insomnia} alt="logo" class="resize h-28 w-28 " />
+          </a>
+          <div className="font-mono text-2xl font-medium"><h1 classname="font-mono"> Insomnia Labs</h1></div>
+          <div className="md:flex items-center space-x-8 hidden m-auto w-2/6">
+            <SectionButton className="text-white" directName="Home" />
+            <SectionButton className="text-white" directName="Products" />
+            <SectionButton className="text-white" directName="Socials" />
+          </div>
+        </div>
+        <div className="flex bg-black h-5/6">
+          <div className="md:flex items-center space-x-8 m-auto relative bottom-32 ">
+            <SectionButton className="flex justify-center items-center py-3 px-6 w-full font-medium text-white bg-gradient-to-t from-green-600 to-purple-500 rounded-full hover:opacity-75 md:w-auto" directName="Purchase" />
+            <SectionButton className="flex justify-center items-center py-3 px-6 w-full font-medium text-white bg-gradient-to-t from-purple-600 to-purple-500 rounded-full hover:opacity-75 md:w-auto" directName="Dashboard" />
+          </div>
         </div>
       </div>
-      <div className="bg-blue-500 h-screen">
-        <div>
-          <SectionButton className="text-white" directName="Home" />
-          <SectionButton className="text-white" directName="Products" />
-        </div>
-      </div>
-    </main>
+    </div>
 
 
   )
