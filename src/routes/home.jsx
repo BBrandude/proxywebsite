416d173882095b16d12da11f2d282ba951dcdad0
BBrandude <brandudeclutch1@gmail.com>
2022-01-30 14:11:50 -0500

    handles stock data

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -10,6 +10,15 @@ import axios from 'axios'
 import botDetection from "../antibot/index.js";
 import LargeButton from "../componenets/LargeButton"
 import SocialsButton from "../componenets/SocialsButton";
+import { trueGray } from "tailwindcss/colors";
+
+
+
+const Results = () => (
+  <div id="results" className="text-white">
+    Some Results
+  </div>
+)
 
 
 
@@ -20,36 +29,36 @@ async function dataCollection() {
 }
 
 
-
-
-
-
 export default function Home() {
+  
+ 
 
   const [prods, setProds] = useState(null)
   const [serverInvite, setServerInvite] = useState(null)
-
+  const [twitterAccount, setTwitterAccount] = useState(null)
   //let renderedOutput;
 
   useEffect(() => {
     dataCollection()
+   
   }, []);
 
   useEffect(() => {
     axios.get("https://boring-mclean-74ab64.netlify.app/.netlify/functions/api/stock").then(response => {
-      setProds(response.data.map(item => <Product productName={item.name} price={"$" + item.price} desc={item.description} />))
+      setProds(response.data.map(item => <Product productName={item.name} price={item.price} desc={item.description} stock={item.stock} itemLink={item.purchaseLink}/>))
     });
   }, []);
 
   useEffect(() => {
-    axios.get("https://boring-mclean-74ab64.netlify.app/.netlify/functions/api/discord").then(response => {
+    axios.get("https://boring-mclean-74ab64.netlify.app/.netlify/functions/api/media").then(response => {
       console.log(response.data)
-      setServerInvite(response.data)
+      setServerInvite(response.data.discord)
+      setTwitterAccount(response.data.twitter)
     });
   }, []);
 
   return (
-    <div className="scrollbar scrollbar-thumb-custom scrollbar-track-custom-light overflow-y-scroll">
+    <div>
       <div className="resize-none">
         <div className="flex-grow flex items-center scroll bg-black text-white h-1/6">
           <div className="flex items-center ml-8">
@@ -61,7 +70,7 @@ export default function Home() {
           <div className="hidden lg:inline-block lg:space-x-28 lg:m-auto lg:pr-80">
             <SectionButton directName="Home" />
             <SectionButton directName="Products" href={"/#products"} />
-            <SectionButton directName="Socials" />
+            <SectionButton directName="Socials" href={"/#socials"}/>
           </div>
           <div className="absolute right-0">
             <a href={serverInvite} class="flex items-center space-x-3 lg:pr-52">
@@ -84,7 +93,7 @@ export default function Home() {
       </div>
       <div className="bg-gradient-to-t from-black to-indigo-500 h-max pb-20" id="products">
         <h1 className="text-white text-center font-mono text-4xl pb-ptt pt-5 ">Products</h1>
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-6 justify-center pt-6">
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:mx-6 md:mx-6 m-20 justify-center pt-6">
           {prods}
         </div>
 
@@ -92,8 +101,8 @@ export default function Home() {
       <div className="flex items-center justify-center h-hm bg-gradient-to-b from-black to-indigo-500">
         <div className="bg-indigo-700 rounded-2xl px-20 py-20 resize-x">
           <div className="resize-none flex flex-row space-x-10">
-            <h1 className="text-white text-lg font-bold pl-5 pt-3">Socials</h1>
-            <SocialsButton directName={'Twitter'} socialsImage={twitter} />
+            <h1 className="text-white text-lg font-bold pl-5 pt-3" id="socials">Socials</h1>
+            <SocialsButton directName={'Twitter'} socialsImage={twitter} href={twitterAccount}/>
             <SocialsButton directName={'Discord'} socialsImage={discord2} href={serverInvite} />
           </div>
         </div>
